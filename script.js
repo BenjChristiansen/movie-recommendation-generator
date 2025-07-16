256da6ec1351b9eb2d01f89a2e0fedd8b72d5fec
BenjChristiansen <128442458+BenjChristiansen@users.noreply.github.com>
2025-07-16 02:45:30 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+let movies = [
+  { title: "Mean Girls", 
+genre: "Comedy",
+description: "Cady Heron is a hit with The Plastics, the A-list girl clique at her new school, until she makes the mistake of falling for Aaron Samuels, the ex-boyfriend of alpha Plastic Regina George."
+},
+  { title: "The Shining", 
+genre: "Horror",
+description: "A family heads to an isolated hotel for the winter, where a sinister presence influences the father into violence. At the same time, his psychic son sees horrifying forebodings from both the past and the future."
+},
+  { title: "Alien", 
+genre: "Horror",
+description: "After investigating a mysterious transmission of unknown origin, the crew of a commercial spacecraft encounters a deadly lifeform."
+},
+  { title: "The Wizard of Oz", 
+genre: "Musical",
+description: "Young Dorothy Gale and her dog Toto are swept away by a tornado from their Kansas farm to the magical Land of Oz and embark on a quest with three new friends to see the Wizard, who can return her to her home and fulfill the others' wishes."
+},
+  { title: "Look Into My Eyes", 
+genre: "Documentary",
+description: "A documentary through a series of intimate sessions with psychics and their clients."
+}
+  function getRandomMovie(movies){
+  const randomIndex = Math.floor(Math.random() * movies.length);
+  return movies[randomIndex];
+  }
+  
